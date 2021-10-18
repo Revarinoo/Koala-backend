674018fe35a20ff3efc02c34fe1385c92e3401bf
revarinoo <revarinoputra@gmail.com>
2021-10-18 13:26:02 +0700

    Merge to login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Business;
+use App\Models\Influencer;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+           
+            $user = auth()->user();
+            if ($request['type_role'] == "Business") {
+                
+                $business_owner = Business::where('user_id', $user->id)->first();
+                
+                if ( $business_owner != null){
+                    $token = $user->createToken('auth_token')->plainTextToken;
+                    $responses = [
+                        'business_owner'=>$business_owner,
+                        'token'=>$token
+                    ];
+                    return response($responses, 201);
+                } 
+            }else if ($request['type_role'] == "Influencer") {
+               
+                $influencer = Influencer::where('user_id', $user->id)->first();
+               
+                if ( $influencer != null){
+                    $token = $user->createToken('auth_token')->plainTextToken;
+                    $responses = [
+                        'influencer'=>$influencer,
+                        'token'=>$token
+                    ];
+                    return response($responses, 201);
+                }
+            }
+        }
+        return response()->json([
+            'message' => 'The provided credentials do not match our records.'
+        ], 401);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json(['message'=>'Logged out'], 201);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\InfluencerController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [RegisterController::class, 'registerUser']);
 Route::get('/influencers', [InfluencerController::class, 'getAllInfluencer']);
 Route::get('/influencer/rate/{influencer}', [InfluencerController::class, 'getMinRate']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout']);
 });
