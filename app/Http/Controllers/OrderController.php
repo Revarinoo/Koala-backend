ab9bfe328565a56b387d0f211a264cd6fc1bf32c
revarinoo <revarinoputra@gmail.com>
2021-11-22 18:48:16 +0700

    Create getOrderList for influencer

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -118,6 +118,30 @@ class OrderController extends Controller
             'message'=>"Success"
         ]);
     }
+
+    /* Influencer */
+    public function orderList() {
+        $influencer = auth()->user()->influencer->id;
+        $orders = Order::where('influencer_id', $influencer)->get();
+        $data = array();
+        foreach ($orders as $order) {
+            array_push($data, [
+                'order_id'=> $order->id,
+                'content_id'=> $order->content->id,
+                'status'=> $order->status,
+                'campaign_name'=>$order->content->name,
+                'start_date'=> date("d-m-Y", strtotime($order->content->start_date)),
+                'end_date'=> date("d-m-Y", strtotime($order->content->end_date)),
+                'photo'=> Utility::$imagePath . $order->content->campaign_logo
+            ]);
+        }
+
+        return response()->json([
+            'code'=>201,
+            'message'=>"Success",
+            'data' => $data
+        ]);
+    }
 }
 
 class BusinessOrder {
