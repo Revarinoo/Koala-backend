266fed88c7840d078be10250cba945e4feface9c
revarinoo <revarinoputra@gmail.com>
2021-11-23 23:25:40 +0700

    Update image path

--- app/Models/Utility.php
+++ app/Models/Utility.php
@@ -3,6 +3,6 @@
 namespace App\Models;
 
 class Utility {
-//static $imagePath = "http://34.124.208.0/Koala-backend/public/storage/images/";
-static $imagePath = "http://127.0.0.1:8000/storage/images/";
+static $imagePath = "https://koala-influencer.xyz/storage/images/";
+//static $imagePath = "http://127.0.0.1:8000/storage/images/";
 }
