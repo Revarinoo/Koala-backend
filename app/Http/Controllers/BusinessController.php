3d4a46eac0c8abbe986fbb69a3c97a9cb799e819
Clara Anggraini <claraanggraini_01@yahoo.com>
2021-11-27 08:21:45 +0700

    add update business profile

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -8,5 +8,30 @@ use Illuminate\Support\Facades\Storage;
 
 class BusinessController extends Controller
 {
+    function updateBusinessProfile(Request $request) {
+        $input = $request->all();
+        $user = auth()->user();
+        $business = Business::where('user_id', $user->id);
 
+        if($business != null){
+            if ($file = $request->file('image')) {
+                if ($business->business_photo != null) Storage::delete('public/images/'. $business->business_photo);
+                $imgname = time() . $file->getClientOriginalName();
+                Storage::putFileAs('public/images',$file,$imgname);
+                $input['image'] = $imgname;
+            }
+
+            $business->update($input);
+
+            $user->update($input->description);
+            return response()->json([
+                'code'=>201,
+                'message' => 'success'
+            ], 201);
+        }
+        return response()->json([
+            'code'=>401,
+            'message' => 'failed'
+         ], 401);
+    }
 }
